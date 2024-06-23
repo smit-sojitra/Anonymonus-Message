4037601ea43f49f9922d6d181e97da4c7519f8c3
Smit Sojitra <smitsojitra810@gmail.com>
2024-06-23 18:30:39 +0530

    update

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,22 +1,45 @@
-// import { openai } from '@ai-sdk/openai';
-// import { streamText } from 'ai';
+// // import { openai } from '@ai-sdk/openai';
+// // import { streamText } from 'ai';
 
-// // Allow streaming responses up to 30 seconds
-// export const maxDuration = 30;
+// // // Allow streaming responses up to 30 seconds
+// // export const maxDuration = 30;
 
-// export async function POST(req: Request) {
-//     try {
-//         // const { messages } = await req.json();
-//         const prompt = "tell jokes"
-//         // "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
-//         const result = await streamText({
-//             model: openai('gpt-3.5-turbo-instruct'),
-//             prompt,
-//         });
+// // export async function POST(req: Request) {
+// //     try {
+// //         // const { messages } = await req.json();
+// //         const prompt = "tell jokes"
+// //         // "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+// //         const result = await streamText({
+// //             model: openai('gpt-3.5-turbo-instruct'),
+// //             prompt,
+// //         });
        
-//         console.log("Result",result);
-//         return result.toAIStreamResponse();
+// //         console.log("Result",result);
+// //         return result.toAIStreamResponse();
         
+// //     } catch (error:any) {
+// //         console.log("Error generating message:-",error)
+// //         return Response.json({
+// //             success:false,
+// //             message:"Error generating message",
+// //             error:error.message,
+// //         })
+// //     }
+// import OpenAi from 'openai'
+
+// const openai = new OpenAi({
+//     apiKey:process.env.OPENAI_API_KEY
+// })
+// export async function POST(request:Request){
+//     try {
+//         const completion = await openai.chat.completions.create({
+//             messages:[{role:'user', content:'Tell me jokes'}],
+//             model:'gpt-3.5-turbo'
+//         })
+//         return Response.json({
+//             success:true,
+//             message:completion
+//         })
 //     } catch (error:any) {
 //         console.log("Error generating message:-",error)
 //         return Response.json({
@@ -25,27 +48,4 @@
 //             error:error.message,
 //         })
 //     }
-import OpenAi from 'openai'
-
-const openai = new OpenAi({
-    apiKey:process.env.OPENAI_API_KEY
-})
-export async function POST(request:Request){
-    try {
-        const completion = await openai.chat.completions.create({
-            messages:[{role:'user', content:'Tell me jokes'}],
-            model:'gpt-3.5-turbo'
-        })
-        return Response.json({
-            success:true,
-            message:completion
-        })
-    } catch (error:any) {
-        console.log("Error generating message:-",error)
-        return Response.json({
-            success:false,
-            message:"Error generating message",
-            error:error.message,
-        })
-    }
-}
+// }
