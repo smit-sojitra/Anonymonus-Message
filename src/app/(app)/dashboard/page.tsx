8f9d3c669a5300d092e63d12f05b9290868469ac
Smit Sojitra <smitsojitra810@gmail.com>
2024-06-23 18:02:15 +0530

    update

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import MessageCard from '@/components/MessageCard';
+import  MessageCard  from '@/components/MessageCard';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { Switch } from '@/components/ui/switch';
