cf450183ad87590a8e47d24fd5b5984b6eda9069
Smit Sojitra <smitsojitra810@gmail.com>
2026-03-15 01:22:10 +0100

    Error solved

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -27,7 +27,7 @@ const Page = () => {
     const [isSwitchLoading, setIsSwitchLoading] = useState(false)
     // const {toast as t} = useToast();
     const handleDeletMessage = (messageId:string)=>{
-        setMessages(messages.filter((message)=>message._id !== messageId))
+        setMessages(messages.filter((message) => (message._id as string).toString() !== messageId))
     }
     const{data:session} = useSession();
     const form = useForm({
